62db87f79c3163706fce5d605a6567b76870f6f8
TheG1uy <paulnvidia@gmail.com>
2017-12-22 10:27:16 +0300

    V_first_list

---
+++ include/TList.h
@@ -0,0 +1,153 @@
+#pragma once
+#include <iostream>
+using namespace std;
+template <class T>
+struct TLink {
+	T val;
+	TLink *pNext;
+};
+template <class T> 
+class TList{
+	TLink<T> *pFirst, *pLast, *pPrev, *pCurr, *pStop;
+	int size, pos;
+public:
+	TList(){
+		pFirst=pLast=pCurr=pStop=NULL;
+		size=0;
+		pos=-1;
+	}
+	void sortInput(T a){
+		if (pFirst==pStop || a<pFirst->val ) { addFirst(a); return ;}
+		if (a>pLast->val){ addLast(a); return ;}
+		for(reset();!isEnd();goNext()){
+			if (pCurr->val>a) {addCurr(a); return; }
+		}
+
+		
+
+	}
+	void sotrB(){
+		
+		T tmp;
+		for (int i=0;i<size-1;i++){
+			reset();
+			for(int j=0;j<size-1-i;j++){
+				goNext();
+				if (pPrev->val>pCurr->val){
+					tmp=pCurr->val;
+					pCurr->val=pPrev->val;
+					pPrev->val=tmp;
+				}
+			}
+		}
+	}
+	void addFirst (T a){
+		TLink<T> *tmp=new TLink <T>;
+		tmp->val=a;
+		if (size==0)
+		{
+			pFirst=pLast=pCurr=tmp;
+			tmp->pNext=pStop;
+		}
+		else
+		{
+			tmp->pNext=pFirst;
+			pFirst=tmp;
+            if(size==1) pPrev=pFirst;
+		}
+		
+		pos++;
+		size++;
+	}
+	void addCurr (T a){
+		if (pFirst==pCurr) addFirst(a);
+		else 
+		{
+
+			TLink<T> *tmp=new TLink<T>;
+			tmp->val=a;
+			tmp->pNext=pCurr;
+			pPrev->pNext=tmp;
+			pCurr=tmp;
+			size++;
+		}
+		
+	}
+	void addLast (T a){
+		if (pFirst==pStop) addFirst(a);
+		else{
+             TLink<T> *tmp=new TLink<T>;
+			 tmp->val=a;
+			 pLast->pNext=tmp;
+			 tmp->pNext=pStop;
+             pLast=tmp;
+			 size++;						
+		}
+		
+	}
+	T getElelm(){ return pCurr->val; }
+	void delFirst (){
+		if (size==1)
+		{
+			delete pFirst;
+			pFirst=pLast=pPrev=pCurr=pStop;
+		}
+		else
+		{
+			TLink<T> *tmp=pFirst->pNext;
+			delete pFirst;
+			pFirst=tmp;
+		}
+		size--;
+		pos--;
+	}
+	void delCurr (){
+		if (pCurr==pFirst)
+		
+			delFirst();
+		
+		else 
+		{
+			pCurr=pCurr->pNext;
+			delete pPrev->pNext;
+			pPrev->pNext=pCurr;
+			size--;
+		}
+	}
+	void delLast (){
+	  if (pLast==pFirst) delFirst();
+	  else{
+		  for(reset();!(pCurr->pNext==pStop);goNext()){}
+		  delete pLast;
+		  pLast=pCurr=pPrev;
+	      reset();
+		  size--;
+		  pos--;
+	  }
+	}
+	void reset(){
+		pPrev=pCurr=pFirst;
+		pos=0;
+	}
+	void goNext (){
+		if (pFirst->pNext==pStop) pPrev=pFirst;
+		else{
+		pPrev=pCurr;
+		pCurr=pCurr->pNext;
+		pos++;
+		}
+	}
+	bool isEnd (){
+		return pCurr==pStop;
+	}
+
+	bool isEmpty(){
+		return pFirst==pStop;
+	}
+	T operator[](int m){
+		for(reset();!isEnd();goNext()){
+			if (pos==m-1)
+			return pCurr->val;
+		}
+	}	
+};
---
+++ include/main.cpp
@@ -0,0 +1,22 @@
+#include "TList.h"
+#include <cstdlib>
+#include <time.h>
+int main(){
+	TList<int> a;
+
+	
+	a.sortInput(5);
+	a.sortInput(2);
+	a.sortInput(1);
+	a.sortInput(7);
+	a.sortInput(3);
+	a.sortInput(4);
+	a.sortInput(8);
+	a.sortInput(6);
+	a.sortInput(9);
+	for(a.reset();!a.isEnd();a.goNext()){cout<<a.getElelm()<<endl;}
+	cout<<endl;
+	//a.sotrB();
+	//for(a.reset();!a.isEnd();a.goNext()){cout<<a.getElelm()<<endl;}
+	return 0;
+}
